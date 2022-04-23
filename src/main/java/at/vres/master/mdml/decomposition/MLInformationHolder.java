fc9b45a2cbb35fd252dcf6bfb3caa4774dee50fd
Matthias Rupp <matthiaswilhelm.rupp@v-research.at>
2022-04-23 23:38:31 +0200

    First commit

---
+++ src/main/java/at/vres/master/mdml/decomposition/MLInformationHolder.java
@@ -0,0 +1,63 @@
+package at.vres.master.mdml.decomposition;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MLInformationHolder {
+	private String qualifiedName;
+	private String name;
+	private Map<String, Map<String, Object>> stereotypes = new HashMap<>();
+	private Map<String, Object> properties = new HashMap<>();
+	private Map<String, Object> parts = new HashMap<>();
+
+	public MLInformationHolder() {
+		super();
+	}
+
+	public MLInformationHolder(String qualifiedName, String name) {
+		super();
+		this.qualifiedName = qualifiedName;
+		this.name = name;
+	}
+
+	public String getQualifiedName() {
+		return qualifiedName;
+	}
+
+	public void setQualifiedName(String qualifiedName) {
+		this.qualifiedName = qualifiedName;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Map<String, Map<String, Object>> getStereotypes() {
+		return stereotypes;
+	}
+
+	public void setStereotypes(Map<String, Map<String, Object>> stereotypes) {
+		this.stereotypes = stereotypes;
+	}
+
+	public Map<String, Object> getProperties() {
+		return properties;
+	}
+
+	public void setProperties(Map<String, Object> properties) {
+		this.properties = properties;
+	}
+
+	public Map<String, Object> getParts() {
+		return parts;
+	}
+
+	public void setParts(Map<String, Object> parts) {
+		this.parts = parts;
+	}
+
+}
